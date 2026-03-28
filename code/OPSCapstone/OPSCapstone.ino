51423708368400e58e4c974c856c89abd05f3580
Johanna H. <johannaalexish@gmail.com>
2026-03-28 12:08:38 -0400

    Main folder
    Initial implementation of the main file for the wireless racing game.

---
+++ code/OPSCapstone/OPSCapstone.ino
@@ -0,0 +1,18 @@
+
+// Main file for the wireless racing game
+
+#include "config.h"
+#include "sprites.h"
+#include "comms.h"
+#include "game_logic.h"
+#include "mechanics.h"
+
+void setup() {
+  Serial.begin(9600);
+  // TODO: Add setup code later
+  Serial.println("Race is Starting...");
+}
+
+void loop() {
+  // TODO: Main game loop will go here
+}
