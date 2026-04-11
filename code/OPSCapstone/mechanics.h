bb60b4f2965ec9185619ee84a9f4ccbe505a48b0
Johanna H. <johannaalexish@gmail.com>
2026-04-11 17:00:53 -0400

    Updated mechanics.h

--- code/OPSCapstone/mechanics.h
+++ code/OPSCapstone/mechanics.h
@@ -1,45 +1,53 @@
 // mechanics.h
-// Power-ups, sabotage, scoring, and game mechanics
+// Power-ups, sabotage, scoring, and point system
 
 #pragma once
 #include "config.h"
 
-extern int score;
+extern int playerScore;
+extern int opponentScore;
 extern bool sabotaged;
 extern unsigned long sabotageEndTime;
 
 void initMechanics() {
-  score = 0;
+  playerScore = 0;
+  opponentScore = 0;
   sabotaged = false;
 }
 
-void updateMechanics() {
-  updateSabotage();
+// Add points for different actions
+void addPoints(int points) {
+  playerScore += points;
+  Serial.print("Points added: +");
+  Serial.println(points);
 }
 
-// Handle when player gets sabotaged
+// Check and collect power-up
+bool checkPowerUpCollection(int playerLane) {
+  // This will be expanded with actual power-up position later
+  if (random(0, 100) < 6) {        // Chance to collect
+    addPoints(25);
+    return true;
+  }
+  return false;
+}
+
+// When you successfully sabotage the opponent
+void onSuccessfulSabotage() {
+  addPoints(15);
+}
+
+// When you get sabotaged (penalty)
 void triggerSabotage() {
   sabotaged = true;
   sabotageEndTime = millis() + SABOTAGE_DURATION;
-  playerProgress = max(0, playerProgress - 15);   // Push back 15 units
-  Serial.println("Sabotage received - pushed back!");
+  playerScore = max(0, playerScore - 10);   // Small penalty
+  playerProgress = max(0, playerProgress - 15);
 }
 
 // Update sabotage timer
 void updateSabotage() {
   if (sabotaged && millis() > sabotageEndTime) {
     sabotaged = false;
-    Serial.println("Sabotage effect ended");
   }
-}
-
-// Simple power-up collection (to be expanded by teammate)
-bool checkPowerUpCollection() {
-  // Random chance for now - teammate can improve this
-  if (random(0, 100) < 5) {   // 5% chance per loop
-    score += 10;
-    Serial.println("Power-up collected!");
-    return true;
-  }
-  return false;
 }
